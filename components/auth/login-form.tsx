2a9c93dd2c0bc113ddb5bdba1313774e54736edc
Wael <waelassaf121@gmail.com>
2024-06-30 06:17:24 +0300

    Create Login ForM

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,26 @@
 "use client"
 
 import CardWrapper from "@/components/auth/card-wrapper"
+import { FormError } from "@/components/form-error"
+import { FormSuccess } from "@/components/form-success"
+import * as z from "zod"
+import { LoginSchema } from "@/schemas"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 export const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+    })
+
+    const onSubmit = (data: z.infer<typeof LoginSchema>) => {
+        console.log(data)
+    }
+
     return (
         <CardWrapper
             headerLabel="Welcome back!"
@@ -10,7 +28,46 @@ export const LoginForm = () => {
             backButtonHref="/auth/register"
             showSocial={true}
         >
-            Login Form
+            <form className="space-y-4 mb-5" onSubmit={handleSubmit(onSubmit)}>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text"> Email</span>
+                    </div>
+                    <input
+                        type="text"
+                        className="input input-bordered w-full"
+                        placeholder="Type Your Email"
+                        {...register("email")}
+                    />
+                    {errors?.email?.message && (
+                        <p className="text-red-700 mb-4">
+                            {errors.email.message}
+                        </p>
+                    )}
+                </label>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">Password</span>
+                    </div>
+                    <input
+                        type="password"
+                        className="input input-bordered w-full"
+                        placeholder="Type Your Password"
+                        {...register("password")}
+                    />
+                </label>
+                {errors?.password?.message && (
+                    <p className="text-red-700 mb-4">
+                        {errors.password.message}
+                    </p>
+                )}
+
+                <FormError message="" />
+                <FormSuccess message="" />
+                <button className="btn btn-neutral w-full" type="submit">
+                    Login
+                </button>
+            </form>
         </CardWrapper>
     )
 }
---
+++ schemas/index.ts
@@ -0,0 +1,15 @@
+import * as z from "zod"
+
+export const LoginSchema = z.object({
+    email: z.string().email({
+        message: "Email is required!",
+    }),
+    password: z.string().min(1, {
+        message: "Password is required!",
+    }),
+})
+
+// export const RegisterSchema = z.object({
+//     email: z.string().email(),
+//     password: z.string(),
+// })
